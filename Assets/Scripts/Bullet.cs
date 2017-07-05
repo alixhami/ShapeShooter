8a8ab73f0cc7147338889b92ba84ea562ce42b03
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-05 12:27:58 -0700

    objects are destroyed when they leave the boundary

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Shapes {
-  public class Bullet : MonoBehaviour {
-    
-    void OnCollisionEnter (Collision c) {
-      transform.SetParent(c.gameObject.transform);
-      GetComponent<Rigidbody>().isKinematic = true;
-    }
 
-    void selfDestruct () {
-      Destroy(gameObject);
-    }
-  }  
+public class Bullet : MonoBehaviour {
+  
+  void OnCollisionEnter (Collision c) {
+    transform.SetParent(c.gameObject.transform);
+    GetComponent<Rigidbody>().isKinematic = true;
+  }
+
+  void selfDestruct () {
+    Destroy(gameObject);
+  }
 }
 
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -2,25 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Shapes {
-  public class Shooter : MonoBehaviour {
 
-    public Transform crosshair;
-    public GameObject projectile;
-    private SteamVR_TrackedController controller;
+public class Shooter : MonoBehaviour {
 
-    void Start () {
-      controller = GetComponentInParent<SteamVR_TrackedController> ();
-      controller.TriggerClicked += Shoot;
+  public Transform crosshair;
+  public GameObject projectile;
+  private SteamVR_TrackedController controller;
+
+  void Start () {
+    controller = GetComponentInParent<SteamVR_TrackedController> ();
+    controller.TriggerClicked += Shoot;
+  }
+
+  void Shoot (object sender, ClickedEventArgs e) {
+    //transform == gameObject.transform == gameObject.GetComponent<Transform>()
+    if (projectile) {
+      GameObject newProjectile = Instantiate(projectile, crosshair.gameObject.transform.position, transform.rotation)as GameObject;
+      newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * 20f, ForceMode.VelocityChange);
     }
-
-    void Shoot (object sender, ClickedEventArgs e) {
-      //transform == gameObject.transform == gameObject.GetComponent<Transform>()
-      if (projectile) {
-        GameObject newProjectile = Instantiate(projectile, crosshair.gameObject.transform.position, transform.rotation)as GameObject;
-        newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * 20f, ForceMode.VelocityChange);
-      }
-    }
-  }  
-}
+  }
+}  
 

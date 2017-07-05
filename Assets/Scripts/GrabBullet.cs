09e111e16a41e2e71cc569d0aa624e631521782e
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-05 14:47:24 -0700

    spheres grab bullets rather than bullets sticking to anything

---
+++ Assets/Scripts/GrabBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabBullet : MonoBehaviour {
+
+  void OnTriggerEnter (Collision c) {
+    if (c.gameObject.tag == "Bullet") {
+      c.transform.SetParent(transform);
+      c.transform.GetComponent<Rigidbody>().isKinematic = true;
+    }
+  }
+}

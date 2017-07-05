53119e6513527ff0acab0ec58c9bbd08f82679a8
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-05 14:29:40 -0700

    random spheres move toward and past the player

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class RandomSpawner : MonoBehaviour {
 
 	public GameObject[] spawnees;
-  public Transform spawnPos;
 
   int randomInt;
 	
 	void Start () {
-    InvokeRepeating("SpawnRandom", 1.0f, 1.0f);
+    InvokeRepeating("SpawnRandom", 1.0f, 2.0f);
 	}
 
   void SpawnRandom () {
     randomInt = Random.Range(0, spawnees.Length);
-    Vector3 randomPos = spawnPos.position;
+
+    // Set random position on x axis, based on position of spawn origin
+    Vector3 randomPos = transform.position;
     randomPos.x = Random.Range(-5f ,5f);
 
-    GameObject newSpawn = Instantiate(spawnees[randomInt], randomPos, spawnPos.rotation)as GameObject;
-    newSpawn.GetComponent<Rigidbody>().AddForce(transform.forward * 20f, ForceMode.VelocityChange);
+    GameObject newSpawn = Instantiate(spawnees[randomInt], randomPos, transform.rotation)as GameObject;
+    newSpawn.GetComponent<Rigidbody>().AddForce(newSpawn.transform.forward * 1f, ForceMode.VelocityChange);
   }
 }

f52d01639138a1e5db5fb692fa2e3dd1cd39ec89
Alix Hamilton <alix.hamilton@gmail.com>
2017-07-05 12:14:37 -0700

    target and enemy spheres spawn rapidly and shoot out of the wall plane

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 namespace Shapes {
   public class Bullet : MonoBehaviour {
-
-    void Start () {
-      
-    }
     
     void OnCollisionEnter (Collision c) {
       transform.SetParent(c.gameObject.transform);
---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour {
+
+	public GameObject[] spawnees;
+  public Transform spawnPos;
+
+  int randomInt;
+	
+	// Update is called once per frame
+	void Update () {
+		SpawnRandom();
+	}
+
+  void SpawnRandom () {
+    randomInt = Random.Range(0, spawnees.Length);
+    GameObject newSpawn = Instantiate(spawnees[randomInt], spawnPos.position, spawnPos.rotation)as GameObject;
+    newSpawn.GetComponent<Rigidbody>().AddForce(transform.forward * 20f, ForceMode.VelocityChange);
+  }
+}
